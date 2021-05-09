dee18ff0b2a20c70d0c867e710fdb0d0ae73590a
iamKAviB <iamkavib@gmail.com>
2021-05-09 16:02:51 +0530

    grocery(model-commit3)

---
+++ app/src/main/java/com/example/finalchopchop_recipeapp/TestJava.java
@@ -0,0 +1,18 @@
+package com.example.finalchopchop_recipeapp;
+
+public class TestJava {
+
+    public String getIn() {
+        return in;
+    }
+
+    public void setIn(String in) {
+        this.in = in;
+    }
+
+    private String in;
+
+    public TestJava(){}
+
+
+}
